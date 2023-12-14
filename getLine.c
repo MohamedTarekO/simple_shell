dd71025713339c7b0bdbcf2bb6c21978565569fe
MohamedTarekO <moh.tarek2999@gmail.com>
2023-12-13 17:52:14 -0800

    222

--- getLine.c
+++ getLine.c
@@ -129,46 +129,42 @@ ssize_t read_buf(info_t *info, char *buf, size_t *i)
  */
 int _getline(info_t *info, char **ptr, size_t *length)
 {
-static char buf[READ_BUF_SIZE];
-static size_t i, len;
-size_t k;
-ssize_t r = 0, s = 0;
+	static char buf[READ_BUF_SIZE];
+	static size_t i, len;
+	size_t k;
+	ssize_t r = 0, s = 0;
+	char *p = NULL, *new_p = NULL, *c;
 
-if (i == len)
-{
-i = len = 0;
-}
-r = read_buf(info, buf, &len)
-if (r == -1 || (r == 0 && len == 0))
-{
-return (-1);
-}
+	p = *ptr;
+	if (p && length)
+		s = *length;
+	if (i == len)
+		i = len = 0;
 
-char *p = *ptr, *c = _strchr(buf + i, '\n');
-k = c ? 1 + (unsigned int)(c - buf) : len;
+	r = read_buf(info, buf, &len);
+	if (r == -1 || (r == 0 && len == 0))
+		return (-1);
 
-char *new_p = _realloc(p, s, s ? s + k : k + 1);
-if (!new_p)
-{
-return (p ? free(p), -1 : -1);
-}
+	c = _strchr(buf + i, '\n');
+	k = c ? 1 + (unsigned int)(c - buf) : len;
+	new_p = _realloc(p, s, s ? s + k : k + 1);
+	if (!new_p) /* MALLOC FAILURE! */
+		return (p ? free(p), -1 : -1);
 
-if (s)
-{
-	_strncat(new_p, buf + i, k - i);
-}
-else
-{
-_strncpy(new_p, buf + i, k - i + 1);
-}
+	if (s)
+		_strncat(new_p, buf + i, k - i);
+	else
+		_strncpy(new_p, buf + i, k - i + 1);
 
-s += k - i;
-i = k;
-*ptr = p = new_p;
-*length = s;
-return (s);
-}
+	s += k - i;
+	i = k;
+	p = new_p;
 
+	if (length)
+		*length = s;
+	*ptr = p;
+	return (s);
+}
 
 /**
  * sigintHandler - blocks ctrl-C
